8ac72d61ad09192ada9d9d87d126677f9954d64e
Phong Phong <phongphong@Phongs-MacBook-Pro.local>
2024-03-23 00:14:22 +0700

    fix service

--- BE/src/services/todo.service.js
+++ BE/src/services/todo.service.js
@@ -4,29 +4,14 @@ exports.create = async (data) => Todo.create(data);
 
 exports.findOneById = async (todoId) => Todo.findById(todoId);
 
-exports.findAll = async (f, q, k, l) => Todo.find(f).sort(q).skip(k).limit(l);
-
-exports.deleteAllTodos = async () => Todo.deleteMany({});
-
-exports.countDocuments = async () => Todo.countDocuments();
-
 exports.updateById = async (todoId, data) =>
     Todo.findByIdAndUpdate(todoId, data, { new: true });
 
 exports.deleteById = async (todoId) =>
     Todo.findByIdAndDelete(todoId, { new: true });
 
-exports.insertMany = async (data) => Todo.insertMany(data, { new: true });
-
 exports.getAllTodos = async () => Todo.find();
-
-exports.findFilter = async (data, select) => Todo.find(data).select(select);
 
 exports.findOneAndUpdate = async (data, update) =>
     Todo.findOneAndUpdate(data, update, { new: true });
 
-exports.upsertData = async (filter, data) =>
-    Todo.updateOne(filter, { $push: data });
-
-exports.deleteDataFromArray = async (filter, data) =>
-    Todo.updateOne(filter, { $pull: data });
